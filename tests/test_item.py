041d1bfe8ea262903ce3d0090e774728601149b9
Almir <almiruwow@gmail.com>
2024-03-06 22:03:21 +0300

    Дописал тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,7 +8,20 @@ def class_fixture():
     return Item(name='Ноутбук', price=10000, quantity=1000)
 
 
+@pytest.fixture
+def new_fixture():
+    value = Item(name='Смартфон', price=10000, quantity=10)
+    value.name = 'СуперСмартфон'
+    return value
+
+
 def test_item(class_fixture):
     assert class_fixture.name == 'Ноутбук'
     assert class_fixture.calculate_total_price() == 10000000
     assert class_fixture.all[0] == class_fixture
+
+
+def test_new_methods(new_fixture):
+    assert len(new_fixture.name) == 10
+    assert new_fixture.name == 'СуперСмарт'
+    assert new_fixture.string_to_number('5') == 5
